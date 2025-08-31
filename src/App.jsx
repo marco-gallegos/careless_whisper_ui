88b516b0520b7f6f877876cc109e21bb3e0a3416
Marco A. Gallegos <ma_galeza@hotmail.com>
2025-08-30 22:30:23 -0600

    chore(ui): vibed fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Container, Row, Col, Navbar } from 'react-bootstrap'
-import AudioRecorder from './components/AudioRecorder'
-import TranslationHistory from './components/TranslationHistory'
-import ExportOptions from './components/ExportOptions'
-import ErrorBoundary from './components/ErrorBoundary'
-import { AudioTranslationProvider } from './context/AudioTranslationContext'
-import './App.css'
+import { Container, Row, Col, Navbar } from "react-bootstrap";
+import AudioRecorder from "./components/AudioRecorder";
+import TranslationHistory from "./components/TranslationHistory";
+import ExportOptions from "./components/ExportOptions";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { AudioTranslationProvider } from "./context/AudioTranslationContext";
+import "./App.css";
 
 function App() {
   return (
@@ -13,12 +13,10 @@ function App() {
         <div className="App">
           <Navbar bg="primary" variant="dark" className="mb-4">
             <Container>
-              <Navbar.Brand>
-                🎤 Audio Translator
-              </Navbar.Brand>
+              <Navbar.Brand>🎤 Audio Translator</Navbar.Brand>
             </Container>
           </Navbar>
-          
+
           <Container>
             <Row>
               <Col lg={6}>
@@ -37,7 +35,7 @@ function App() {
         </div>
       </AudioTranslationProvider>
     </ErrorBoundary>
-  )
+  );
 }
 
-export default App
+export default App;
